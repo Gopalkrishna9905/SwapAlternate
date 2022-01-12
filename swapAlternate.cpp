be42a47f2f19947e2274c5d4ca0a976dd5805917
Gopalkrishna9905 <88495457+Gopalkrishna9905@users.noreply.github.com>
2022-01-12 19:21:05 +0530

    Add files via upload

---
+++ swapAlternate.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+
+void printArray(int arr[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}cout<<endl;
+}
+
+
+void swapAlternate(int arr[],int size)
+{
+	for(int i=0;i<size;i+=2)
+	{
+		if(i+1<size)
+		{
+			swap(arr[i],arr[i+1]);
+		}
+	}
+}
+
+
+int main()
+{
+	int even[6]={1,2,3,4,5,6};
+	int odd[7]={1,2,3,4,5,6,7};
+	
+	swapAlternate(even,6);
+	
+	printArray(even,6);
+	
+	cout<<endl;
+	
+	swapAlternate(odd,7);
+	
+	printArray(odd,7);
+}
